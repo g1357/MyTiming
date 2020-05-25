f2998e075a5005955231fa50ef4a9a255b7eda55
GBD <Dukarevich_G@EplusE.ru>
2020-05-25 20:00:02 +0300

    Add send e-mail.

---
+++ MyTiming/MyTiming/Views/SendMailPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using MyTiming.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MyTiming.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class SendMailPage : ContentPage
+    {
+        SendMailViewModel viewModel;
+
+        public SendMailPage()
+        {
+            InitializeComponent();
+
+            BindingContext = viewModel = new SendMailViewModel(this);
+        }
+    }
+}
